ff90074677efd4f21b2649ba03b88047d2787707
Morrowsent <100018880+Morrowsent@users.noreply.github.com>
2023-01-27 15:55:01 +0100

    Create 12: Highly divisible triangular number.java

---
+++ 12: Highly divisible triangular number.java
@@ -0,0 +1,22 @@
+public class Euler1 {
+    public static void main(String[] args) {
+        long triangle = 0;
+        for(long i = 10465; i < 90000; i++){
+            long sum = 0;
+            long numDivisor = 0;
+            for(long j = 1; j < i; j++){
+                sum += j;
+            }
+            for(long j = 1; j < sum/2; j++){
+                if(sum % j == 0){
+                    numDivisor++;
+                }
+            }
+            numDivisor++;
+            if(numDivisor > 300){
+                System.out.println(sum + ": " + numDivisor + ": " + i);
+            }
+        }
+    }
+}
+// Solution: 76576500
